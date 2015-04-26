d8b6c34ab1a57990c113a61f6b217267d8abaccd
CQY-ASUS-Ubuntu <pku_cqy@126.com>
2015-04-26 14:12:37 +0800

    add _messageReceived

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,24 @@ require("cloud/app.js");
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello Sixinwen!");
 });
+
+AV.Cloud.define("_messageReceived", function(request, response){
+	var convId = request.params.convId;
+	var fromPeer = request.params.fromPeer;
+	var content = request.params.content;
+
+	var query = new AV.Query("_Conversation");
+	query.get(convId, {
+	      success: function(conversation) {
+	          var x = 1;
+	          if(x == 0){
+	             response.success();
+	          } else {
+	             response.success({"drop":"truthy"}); 
+	          }
+	      },
+	      error: function(object, error) {
+	          response.error("_messageReceived query conversation error!");
+	      }
+	    });
+})
