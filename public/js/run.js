76ee096bf6663b7c11083bdd98b9a037e3cdf77c
CQY-ASUS-Ubuntu <pku_cqy@126.com>
2015-04-27 13:56:39 +0800

    .

--- public/js/run.js
+++ public/js/run.js
@@ -2,7 +2,7 @@
 var appId = 'epg58oo2271uuupna7b9awz9nzpcxes870uj0j0rzeqkm8mh';
 var roomId = '5535e6dde4b078a907134b9f'
 // 每个客户端自定义的 id
-var clientId = 'walker';
+var clientId = 'cqyx';
 var rt;
 var conv;
 var convOld;
