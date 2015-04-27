042136ce4b9b770b7062aa6985f18b0eb650e856
QingYing Chen <pkudysj@126.com>
2015-04-27 23:05:52 +0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,7 @@ require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
+	console.log(request.params);
   response.success("Hello Sixinwen!");
 });
 
