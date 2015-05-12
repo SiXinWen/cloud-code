dce22859bbf0c929e5033d0f9bbdc49c4f6d1d2b
Shuhao Zhang <zsh19930510@163.com>
2015-05-12 10:26:33 +0800

    TestTag

--- cloud/InsSignUp.js
+++ cloud/InsSignUp.js
@@ -4,26 +4,29 @@ AV.Cloud.define('InsSignUp', function(request, response) {
 	var UserName = request.params.InsID;
 	var NewUser = new AV.User();
 	var IfSuc = false;
-	
+	console.log("Tag1");
 	//sign up 
 	//username = InsID password = password
 	NewUser.set("username", UserName);
 	NewUser.set("password", "password");
-	
+	console.log("Tag2");
     user.signUp(null, 
 	{
 		success: function(NewUser) 
 		{
 			ifSuc = true;
+			console.log("Tag3");
 		},
 		error: function(NewUser, error) 
 		{
 			// Show the error message somewhere and let the user try again.
 			alert("Error: " + error.code + " " + error.message);
+			console.log("Tag4");
 		}
 	});
 	var ReturnValue = {
 		result : IfSuc
 	};
+	console.log("Tag5");
 	response.success(ReturnValue);
 });
--- cloud/InsSignUpTest.js
+++ cloud/InsSignUpTest.js
@@ -1,10 +1,15 @@
-AV.Cloud.define('InsSignUpTest', function(request, response) {
-AV.Cloud.run('InsSignUp', {InsID: 'DerekLH'}, {
+AV.initialize("epg58oo2271uuupna7b9awz9nzpcxes870uj0j0rzeqkm8mh", "xjgx65z5yavhg8nj4r48004prjelkq0fzz9xgricyb2nh0qq");
+
+//AV.Cloud.define('InsSignUpTest', function(request, response) {
+AV.Cloud.run('InsSignUp', {'InsID': 'DerekLH'}, {
   success: function(data){
-      console.log("success");
+      alert("success");
   },
   error: function(err){
-      console.log("fail")
+      alert("fail");
   }
 });
-});
+//});
+
+
+
