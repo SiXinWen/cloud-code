ae34215422709b718023c55dafd12bea6e2d05e9
Shuhao Zhang <zsh19930510@163.com>
2015-05-12 10:07:58 +0800

    Push InsSignUp To Main

--- cloud/main.js
+++ cloud/main.js
@@ -120,3 +120,34 @@ AV.Cloud.define("_receiversOffline", function(request, response){
 	console.log(request.params);
 	response.success({"offlinePeers":["test"]});
 });
+
+
+//parameter : InsID String
+//ReturnValue: {result : true}
+AV.Cloud.define('InsSignUp', function(request, response) {
+	var UserName = request.params.InsID;
+	var NewUser = new AV.User();
+	var IfSuc = false;
+	
+	//sign up 
+	//username = InsID password = password
+	NewUser.set("username", UserName);
+	NewUser.set("password", "password");
+	
+    user.signUp(null, 
+	{
+		success: function(NewUser) 
+		{
+			ifSuc = true;
+		},
+		error: function(NewUser, error) 
+		{
+			// Show the error message somewhere and let the user try again.
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
+	var ReturnValue = {
+		result : IfSuc
+	};
+	response.success(ReturnValue);
+});
