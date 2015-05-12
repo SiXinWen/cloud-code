818537f931eeeecc5a69bedf464136dc08a8ec48
CQY-ASUS-Ubuntu <pku_cqy@126.com>
2015-05-12 21:23:25 +0800

    alter some spelling

--- cloud/util.js
+++ cloud/util.js
@@ -9,7 +9,7 @@ module.exports.saveComment = function(params){
             // Execute any logic that should take place after the object is saved.
             //   alert('New object created with objectId: ' + comment.id);
         },
-        error: function(gameScore, error) {
+        error: function(object, error) {
             // Execute any logic that should take place if the save fails.
             // error is a AV.Error with an error code and description.
             //alert('Failed to create new object, with error code: ' + error.message);
